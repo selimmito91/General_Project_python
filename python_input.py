815410e3e6c7e60d5d4172beb6e00a41808a7eb4
selimbd91 <selim.iub@gmail.com>
2020-01-27 05:47:34 +0100

    just testing

---
+++ python_input.py
@@ -0,0 +1,17 @@
+
+def fun():
+
+    name = input("please enter your name:")
+    print("your name is " + name)
+
+def fun2():
+
+    string1 = "amar sonar bangla"
+    print(string1)
+
+def main():
+    fun()
+    fun2()
+
+if __name__ == "__main__":
+    main()
